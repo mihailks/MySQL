dc99fde09b1ff7b88361edc7c53331c6fb7a0cfa
Mihail Slavov <mihail_slavov@icloud.com>
2023-05-28 19:01:37 +0300

    Create Data Aggregation - Exercise.sql

---
+++ Data Aggregation - Exercise/Data Aggregation - Exercise.sql
@@ -0,0 +1,133 @@
+-- 1. Records' Count
+SELECT 
+    COUNT(*)
+FROM
+    wizzard_deposits;
+
+-- 2. Longest Magic Wand
+
+SELECT 
+    MAX(magic_wand_size) AS longest_magic_want
+FROM
+    wizzard_deposits;
+    
+    
+    -- 3. Longest Magic Wand Per Deposit Groups
+select * from wizzard_deposits;
+
+SELECT 
+    deposit_group, MAX(magic_wand_size) AS longest_magic_want
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+ORDER BY longest_magic_want , deposit_group;
+
+-- 4. Smallest Deposit Group Per Magic Wand Size*
+
+SELECT 
+    deposit_group
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+HAVING AVG(magic_wand_size)
+ORDER BY AVG(magic_wand_size)
+LIMIT 1;
+
+-- 5. Deposits Sum
+SELECT 
+    deposit_group, SUM(deposit_amount) AS total_sum
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+ORDER BY total_sum;
+
+-- 6. Deposits Sum for Ollivander Family
+
+SELECT 
+    deposit_group, SUM(deposit_amount) AS total_sum
+FROM
+    wizzard_deposits
+    where magic_wand_creator = 'Ollivander family'
+    group by deposit_group
+    ORDER BY deposit_group;
+    
+ 
+-- 7. Deposits Filter
+
+SELECT 
+    deposit_group, SUM(deposit_amount) AS total_sum
+FROM
+    wizzard_deposits
+WHERE
+    magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+HAVING total_sum < 150000
+ORDER BY total_sum DESC;
+
+
+-- 8. Deposit Charge
+
+SELECT 
+    deposit_group,
+    magic_wand_creator,
+    MIN(deposit_charge) AS 'min_deposit_charge'
+FROM
+    wizzard_deposits
+GROUP BY deposit_group , magic_wand_creator
+ORDER BY magic_wand_creator , deposit_group;
+
+-- 9. Age Groups
+
+SELECT 
+    CASE
+        WHEN age BETWEEN 0 AND 10 THEN '[0-10]'
+        WHEN age BETWEEN 11 AND 20 THEN '[11-20]'
+        WHEN age BETWEEN 21 AND 30 THEN '[21-30]'
+        WHEN age BETWEEN 31 AND 40 THEN '[31-40]'
+        WHEN age BETWEEN 41 AND 50 THEN '[41-50]'
+        WHEN age BETWEEN 51 AND 60 THEN '[51-60]'
+        WHEN age >= 61 THEN '[61+]'
+    END AS 'age_group',
+    COUNT(`first_name`) AS 'wizard_count'
+FROM
+    wizzard_deposits
+GROUP BY `age_group`
+ORDER BY `age_group`;
+
+-- 10. First Letter
+
+SELECT 
+    LEFT(`first_name`, 1) AS 'first_letter'
+FROM
+    wizzard_deposits
+WHERE
+    deposit_group = 'Troll Chest'
+GROUP BY `first_letter`
+ORDER BY `first_letter` ASC;
+
+-- 11. Average Interest
+
+SELECT 
+    deposit_group,
+    is_deposit_expired,
+    AVG(deposit_interest) AS 'average_interest'
+FROM
+    wizzard_deposits
+WHERE
+    deposit_start_date > '1985/01/01'
+GROUP BY deposit_group , is_deposit_expired
+ORDER BY deposit_group DESC , is_deposit_expired ASC;
+
+
+-- 12. Employees Minimum Salaries
+ use soft_uni;
+
+
+
+
+
+
+
+
+
+
