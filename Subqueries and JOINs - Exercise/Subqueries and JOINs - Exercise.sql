399c676f38dacc584d18e3c9f1601c85eaa03819
Mihail Slavov <mihail_slavov@icloud.com>
2023-06-04 15:45:16 +0300

    JOIN

--- Subqueries and JOINs - Exercise/Subqueries and JOINs - Exercise.sql
+++ Subqueries and JOINs - Exercise/Subqueries and JOINs - Exercise.sql
@@ -1,23 +1,223 @@
-use soft_uni;
+USE `soft_uni`;
 
 -- 1. Employee Address
 
-select e.employee_id,
-       e.job_title,
-       a.address_id,
-       a.address_text
-from employees as e
-         join
-     addresses as a on e.address_id = a.address_id
-order by a.address_id asc
-limit 5;
+SELECT `e`.`employee_id`,
+       `e`.`job_title`,
+       `a`.`address_id`,
+       `a`.`address_text`
+FROM `employees` AS `e`
+         JOIN
+     `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+ORDER BY `a`.`address_id`
+LIMIT 5;
 
 -- 2. Addresses with Towns
-select e.first_name, e.last_name, t.name, a.address_text
-from employees as e
-         join addresses as a on e.address_id = a.address_id
-         join towns as t on a.town_id = t.town_id
-order by first_name, last_name
-limit 5;
+SELECT `e`.`first_name`, `e`.`last_name`, `t`.`name`, `a`.`address_text`
+FROM `employees` AS `e`
+         JOIN `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+         JOIN `towns` AS `t` ON `a`.`town_id` = `t`.`town_id`
+ORDER BY `first_name`, `last_name`
+LIMIT 5;
 
 -- 3. Sales Employee
+
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`last_name`, `d`.`name` AS 'department_name'
+FROM `employees` AS `e`
+         JOIN `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE `d`.`department_id` = 3
+ORDER BY `e`.`employee_id` DESC;
+
+-- for Judge
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`last_name`, `d`.`name` AS 'department_name'
+FROM `employees` AS `e`
+         JOIN `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE `d`.`name` = 'Sales'
+ORDER BY `e`.`employee_id` DESC;
+
+-- 04. Employee Departments
+
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`salary`, `d`.`name`
+FROM `employees` AS `e`
+         JOIN `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE `e`.`salary` > 15000
+ORDER BY `d`.`department_id` DESC
+LIMIT 5;
+
+-- 5. Employees Without Project
+
+SELECT *
+FROM `employees`;
+
+-- e.`employee_id`, e.`first_name`
+
+SELECT *
+FROM `employees` AS `e`
+         JOIN `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+         JOIN `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`;
+
+
+SELECT `e`.`employee_id`, `e`.`first_name`
+FROM `employees` AS `e`
+         LEFT JOIN `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+         LEFT JOIN `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
+WHERE `ep`.`project_id` IS NULL
+ORDER BY `e`.`employee_id` DESC
+LIMIT 3;
+
+-- 6. Employees Hired After
+
+SELECT `e`.`first_name`, `e`.`last_name`, `e`.`hire_date`, `d`.`name` AS 'dept_name'
+FROM `employees` AS `e`
+         JOIN `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE `e`.`hire_date` > 1999 / 1 / 1 AND `d`.`name` = 'Sales'
+   OR `d`.`name` = 'Finance'
+ORDER BY `hire_date`;
+
+-- 7. Employees with Project
+
+SELECT `e`.`employee_id`, `e`.`first_name`, `p`.`name` AS 'project_name'
+FROM `employees` AS `e`
+         JOIN `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+         JOIN `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
+WHERE DATE(`p`.`start_date`) > '2002-08-13'
+  AND `p`.`end_date` IS NULL
+ORDER BY `e`.`first_name`, `project_name`
+LIMIT 5;
+
+-- 8. Employee 24
+
+SELECT `e`.`employee_id`,
+       `e`.`first_name`,
+       IF(YEAR(`start_date`) >= 2005, NULL, `p`.`name`) AS 'project_name'
+FROM `employees` AS `e`
+         JOIN `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+         JOIN `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
+WHERE `e`.`employee_id` = 24
+ORDER BY `p`.`name`;
+
+-- 9. Employee Manager
+
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`manager_id`, `m`.`first_name` AS 'manager_name'
+FROM `employees` AS `e`,
+     `employees` AS `m`
+WHERE `e`.`manager_id` = `m`.`employee_id`
+  AND `e`.`manager_id` IN (3, 7)
+ORDER BY `e`.`first_name`;
+
+
+SELECT `e`.`employee_id`,
+       `e`.`first_name`,
+       `e`.`manager_id`,
+       (SELECT `m`.`first_name`
+        FROM `employees` AS `m`
+        WHERE `m`.`employee_id` = `e`.`manager_id`) AS `manager_name`
+FROM `employees` AS `e`
+WHERE `e`.`manager_id` IN (3, 7)
+ORDER BY `e`.`first_name`;
+
+
+-- 10. Employee Summary
+
+SELECT `e`.`employee_id`,
+       CONCAT(`e`.`first_name`, ' ', `e`.`last_name`) AS 'employee_name',
+       CONCAT(`m`.`first_name`, ' ', `m`.`last_name`) AS 'manager_name',
+       `d`.`name`                                     AS 'department_name'
+FROM `employees` AS `e`
+         JOIN `employees` AS `m` ON `e`.`manager_id` = `m`.`employee_id`
+         JOIN `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+ORDER BY `e`.`employee_id`
+LIMIT 5;
+
+
+-- 11. Min Average Salary
+
+
+SELECT AVG(`salary`) AS `min_average_salary`
+FROM `employees`
+GROUP BY `department_id`
+ORDER BY `min_average_salary`
+LIMIT 1;
+
+-- 12. Highest Peaks in Bulgaria
+
+SELECT `c`.`country_code`,
+       `m`.`mountain_range`,
+       `p`.`peak_name`,
+       `p`.`elevation`
+FROM `peaks` AS `p`
+         JOIN `mountains` AS `m` ON `p`.`mountain_id` = `m`.`id`
+         JOIN `mountains_countries` AS `mc` ON `m`.`id` = `mc`.`mountain_id`
+         JOIN `countries` AS `c` ON `mc`.`country_code` = `c`.`country_code`
+WHERE `c`.`country_code` LIKE 'BG'
+  AND `p`.`elevation` > 2835
+ORDER BY `p`.`elevation` DESC;
+
+-- 13. Count Mountain Ranges
+
+SELECT `c`.`country_code`, COUNT(`c`.`country_code`) AS 'mountain_range'
+FROM `countries` AS `c`
+         JOIN `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
+         JOIN `mountains` AS `m` ON `mc`.`mountain_id` = `m`.`id`
+WHERE `c`.`country_code` IN ('BG', 'RU', 'US')
+GROUP BY `c`.`country_code`
+ORDER BY `mountain_range` DESC;
+
+-- 14. Countries with Rivers
+
+SELECT `c`.`country_name`,
+       IF(`river_id` IS NULL, NULL, `r`.`river_name`)
+FROM `countries` AS `c`
+         LEFT JOIN `countries_rivers` AS `cr` ON `c`.`country_code` = `cr`.`country_code`
+         LEFT JOIN `rivers` AS `r` ON `cr`.`river_id` = `r`.`id`
+WHERE `c`.`continent_code` = 'AF'
+ORDER BY `c`.`country_name`
+LIMIT 5;
+
+-- 15. *Continents and Currencies
+
+SELECT `c`.`continent_code`, `currency_code`, COUNT(*) AS 'currency_usage'
+FROM `countries` AS `c`
+GROUP BY `c`.`continent_code`, `c`.`currency_code`
+HAVING `currency_usage` > 1
+   AND `currency_usage` = (SELECT COUNT(*) AS `count_of_currencies`
+                           FROM `countries` AS `c2`
+                           WHERE `c2`.`continent_code` = `c`.`continent_code`
+                           GROUP BY `c2`.`currency_code`
+                           ORDER BY `count_of_currencies` DESC
+                           LIMIT 1)
+ORDER BY `c`.`continent_code`, `c`.`currency_code`;
+
+
+-- 16. Countries Without Any Mountains
+
+SELECT COUNT(*)
+FROM `countries` AS `c`
+         LEFT JOIN `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
+WHERE `mc`.`mountain_id` IS NULL;
+
+-- 17. Highest Peak and Longest River by Country
+
+SELECT `c`.`country_name`,
+       MAX(`p`.`elevation`) AS `highest_peak_elevation`,
+       MAX(`r`.`length`)    AS `longest_river_length`
+FROM `countries` AS `c`
+         LEFT JOIN `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
+         LEFT JOIN `mountains` AS `m` ON `mc`.`mountain_id` = `m`.`id`
+         LEFT JOIN `peaks` AS `p` ON `m`.`id` = `p`.`mountain_id`
+
+         LEFT JOIN `countries_rivers` AS `cr` ON `c`.`country_code` = `cr`.`country_code`
+         LEFT JOIN `rivers` AS `r` ON `cr`.`river_id` = `r`.`id`
+GROUP BY `c`.`country_name`
+ORDER BY `highest_peak_elevation` DESC, `longest_river_length` DESC, `c`.`country_name`
+LIMIT 5;
+
+
+
+
+
+
+
+
+
+
